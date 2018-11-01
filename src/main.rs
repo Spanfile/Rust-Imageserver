1432cbdea8f79ff554e13a6caa3f1adda16ba1c6
Spanfile <spansdev@gmail.com>
2018-11-01 15:49:40 +0200

    haha hyper example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+extern crate futures;
+extern crate hyper;
+
+use futures::future;
+use hyper::rt::{Future, Stream};
+use hyper::service::service_fn;
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+
+type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
+fn echo(req: Request<Body>) -> BoxFut {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        }
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = req.into_body();
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Box::new(future::ok(response))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let server = Server::bind(&addr)
+        .serve(|| service_fn(echo))
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("listening on {}", addr.to_string());
+    hyper::rt::run(server);
 }
